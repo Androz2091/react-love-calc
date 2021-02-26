4a303a98dd9772002a64dba9daa91eb7abf3e7a7
Androz2091 <androz2091@gmail.com>
2021-02-26 12:02:37 +0100

    Clean up UI

--- src/components/Form.js
+++ src/components/Form.js
@@ -50,9 +50,9 @@ const Form = () => {
                 placeholder="Nom 2"
                 className="p-6 text-2xl border-pink rounded focus:outline-none"
             />
-            <p> =</p>
+        </div>
+        <div className="result">
             <h2 className="text-6xl">{percent ? (<CountUp end={percent} />) : '... '}%</h2>
-            <p>d'amour</p>
             <svg class="heart" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style={{ fill: `rgb(255,0,0,${percent / 100})` }}>
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z">
                 </path>
