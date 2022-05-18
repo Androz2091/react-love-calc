120645715f3055dd2b4a618a930a374cf01ab573
Androz <androz2091@gmail.com>
2022-05-18 22:22:38 +0200

    fixes

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ const calculateLove = (firstName, secondName) => {
     if (!firstName.trim().length || !secondName.trim().length) return '';
     const string = [firstName.toLowerCase().trim(), secondName.toLowerCase().trim()].sort((a, b) => b.localeCompare(a)).join('');
     const numbers = MD5(string).toString().split('').filter((c) => !isNaN(c)).reverse();
-    const baseResult = parseInt(numbers[2] + numbers[3]) === 99 ? parseInt(numbers[0]) > 4 ? 100 : 99 : parseInt(numbers[2] + numbers[3]);
+    const baseResult = parseInt(numbers[1] + numbers[2]) === 99 ? parseInt(numbers[0]) > 4 ? 100 : 99 : parseInt(numbers[1] + numbers[2]);
     return baseResult;
 }
 
