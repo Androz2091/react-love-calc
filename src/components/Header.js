a4625392c1a8811d0db3b4270fd05f23960adabc
Simon <androz2091@gmail.com>
2023-09-28 15:39:35 +0200

    changement du titre

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="header">
             <h2 className="text-5xl p-4 text-white font-bold">Love Calculator</h2>
-            <h2 className="text-3xl p-4 text-white">100% vous êtes compatibles, 0% oubliez tout de suite...</h2>
+            <h2 className="text-3xl p-4 text-white">100% vous êtes compatibles, 0% oubliez tout de suite...!</h2>
         </div>
     )
 }
